d38cadbc67e965a108ad8f495ed2422731aeb453
Ebraheem hany <161340238+ebraheemhany@users.noreply.github.com>
2024-06-28 05:24:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let hema_menu = document.getElementById ("hema_menu");
+
+function open_menu(){
+hema_menu.classList.add("active")
+
+
+}
+function close_menu(){
+hema_menu.classList.remove("active")
+
+
+}
+
+
+
+
+
+
+
+
